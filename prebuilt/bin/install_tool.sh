a0c0ef2351ad6b6675c1fd197f6b6d261d8ec911
daedae <daeyoung1112@gmail.com>
2012-11-24 13:47:01 +0900

    modify install_tool.sh

--- prebuilt/bin/install_tool.sh
+++ prebuilt/bin/install_tool.sh
@@ -7,13 +7,7 @@ cp -rp /system/tts/lang_pico /cdrom
 rm /system/tts/lang_pico/*
 ln -s /cdrom/lang_pico/* /system/tts/lang_pico
 
-#symlink kcm for keypad-ru_qwerty.kl
-ln -s /system/usr/keychars/sholes-keypad-qwerty.kcm /system/usr/keychars/sholes-keypad-ru_qwerty.kcm
-
 # clear dex files
 rm /cache/dalvik-cache/*.dex
 mount /data
 rm /data/dalvik-cache/*.dex
-mount /sddata
-rm /sddata/dalvik-cache/*.dex
-
